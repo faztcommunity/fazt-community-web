e08e6ee57c4f0d164796a59b7b81223b1d2fe335
erianvc <erianvc.dev@gmail.com>
2020-08-11 14:12:53 -0500

    feat: add atom description component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Button from '@Atoms/Button';
+import Description from '@Atoms/Description';
 
 export default function Home() {
   return (
@@ -12,6 +13,57 @@ export default function Home() {
       <Button text="Hola Mundo" variant="outline" color="secondary" size="rl" />
       <Button text="Hola Mundo" variant="fill" color="primary" size="sm" />
       <Button text="Hola Mundo" variant="outline" color="primary" size="xsm" />
+
+      <Description>
+        Fazt Community es un espacio donde podrás crecer como profesional, participando en equipos y con
+        personas de todo el mundo hispano hablante.
+      </Description>
+      <Description size="lg">
+        Fazt Community es un espacio donde podrás crecer como profesional, participando en equipos y con
+        personas de todo el mundo hispano hablante.
+      </Description>
+      <Description size="md">
+        Fazt Community es un espacio donde podrás crecer como profesional, participando en equipos y con
+        personas de todo el mundo hispano hablante.
+      </Description>
+      <Description center>
+        Fazt Community es un espacio donde podrás crecer como profesional, participando en equipos y con
+        personas de todo el mundo hispano hablante.
+      </Description>
+      <Description size="lg" center>
+        Fazt Community es un espacio donde podrás crecer como profesional, participando en equipos y con
+        personas de todo el mundo hispano hablante.
+      </Description>
+      <Description size="md" center>
+        Fazt Community es un espacio donde podrás crecer como profesional, participando en equipos y con
+        personas de todo el mundo hispano hablante.
+      </Description>
+      <div style={{ background: '#2C9BEB' }}>
+        <Description white>
+          Fazt Community es un espacio donde podrás crecer como profesional, participando en equipos y con
+          personas de todo el mundo hispano hablante.
+        </Description>
+        <Description size="lg" white>
+          Fazt Community es un espacio donde podrás crecer como profesional, participando en equipos y con
+          personas de todo el mundo hispano hablante.
+        </Description>
+        <Description size="md" white>
+          Fazt Community es un espacio donde podrás crecer como profesional, participando en equipos y con
+          personas de todo el mundo hispano hablante.
+        </Description>
+        <Description center white>
+          Fazt Community es un espacio donde podrás crecer como profesional, participando en equipos y con
+          personas de todo el mundo hispano hablante.
+        </Description>
+        <Description size="lg" center white>
+          Fazt Community es un espacio donde podrás crecer como profesional, participando en equipos y con
+          personas de todo el mundo hispano hablante.
+        </Description>
+        <Description size="md" center white>
+          Fazt Community es un espacio donde podrás crecer como profesional, participando en equipos y con
+          personas de todo el mundo hispano hablante.
+        </Description>
+      </div>
     </>
   );
 }
--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -3,12 +3,19 @@ import { Global, css } from '@emotion/core';
 const GlobalStyles = () => (
   <Global
     styles={css`
+      @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap');
+      @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
+
       *,
       *::after,
       *::before {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+      }
+
+      :root {
+        font-size: 16px;
       }
 
       body {
