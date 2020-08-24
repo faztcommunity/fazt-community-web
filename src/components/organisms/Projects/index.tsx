03a9c75718f7c7f539d505e748aac6dbc116ab15
Evelyn Bonato <evebonato23@gmail.com>
2020-08-24 20:25:30 -0300

    feat(project): added title, description

--- src/components/organisms/Projects/index.tsx
+++ src/components/organisms/Projects/index.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import styled from '@Styles/styled';
 import CardGroupProjects from '@Organisms/CardGroupProjects';
+import Title from '@Atoms/Title';
+import Description from '@Atoms/Description';
 
-const StyleProjects = styled.div``;
+const StyleProjects = styled.div`
+  width: 100%;
+  min-height: calc(100vh - 90px);
+  height: max-content;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.light.color.secondary};
+
+  .descripcion {
+    margin: 50px 0px 50px 0px;
+
+    h1 {
+      margin-bottom: 16px;
+    }
+    p {
+      margin-bottom: 40px;
+    }
+    width: 500px;
+  }
+`;
 
 const Projects: React.FC = () => (
   <StyleProjects>
-    <CardGroupProjects />
-    <div>hola</div>
+    <div className="descripcion">
+      <Title align="center" white size="sub2">
+        Proyectos de la comunidad
+      </Title>
+      <Description size="lg" center white>
+        En la comunidad podrás encontrar distintos proyectos para colaborar en diferentes tecnologias. Tenemos
+        4 proyectos principales que son:
+      </Description>
+      <CardGroupProjects />
+    </div>
   </StyleProjects>
 );
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Projects from '@Organisms/Projects';
+import MainTemplate from '@Templates/MainPage';
+import Head from '@Molecules/Head';
 /* They test their components and at the end clean the Index file */
 
 const Index: React.FC = () => {
   return (
     <>
-      <Projects />
+      <MainTemplate>
+        <Head title="Hola como estas" />
+        <Projects />
+      </MainTemplate>
     </>
   );
 };
