6423b4e7ae1e49bb2ce52626771cbc2b8be817c7
Fernando Lopez <lopezfer@globalhitss.com>
2020-09-12 11:08:26 -0300

    fix axios

--- src/assets/hooks/axios.ts
+++ src/assets/hooks/axios.ts
@@ -2,7 +2,6 @@ import Axios from 'axios';
 
 const instance = Axios.create({
   baseURL: 'https://api.faztcommunity.dev',
-  timeout: 1000,
   headers: {
     'Content-Type': 'application/json'
   }
