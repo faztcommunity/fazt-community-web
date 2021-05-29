7195e75a2bee7d9b8c6e7c2272d46cccc35219f7
erianvc <erianvc.dev@gmail.com>
2021-05-29 01:48:38 -0500

    feat: add button component

---
+++ src/components/Button/index.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let fill = false
+    export let primary = false
+    export let large = false
+    export let medium = false
+    export let small = false
+
+    const btn = true
+</script>
+
+<button
+    class:btn
+    class:fill
+    class:outline={!fill}
+    class:primary
+    class:secondary={!primary}
+    class:large
+    class:medium
+    class:small
+    {...$$restProps}
+    on:click>
+    <slot />
+</button>
+
+<style global src="styles.scss"></style>
---
+++ src/routes/components.svelte
@@ -0,0 +1,26 @@
+<script>
+    import Button from '@components/Button'
+</script>
+
+<section>
+    <article>
+        <Button fill primary large>Unirme a la comunidad</Button>
+        <Button fill large>Ver proyectos</Button>
+        <br /><br />
+        <Button primary large>Unirme a la comunidad</Button>
+        <Button large>Ver proyectos</Button>
+        <br /><br />
+        <Button fill primary large disabled>Unirme a la comunidad</Button>
+        <Button fill large disabled>Ver proyectos</Button>
+        <br /><br />
+        <Button primary large disabled>Unirme a la comunidad</Button>
+        <Button large disabled>Ver proyectos</Button>
+    </article>
+</section>
+
+<style>
+    section {
+        width: 100%;
+        height: 100%;
+    }
+</style>
