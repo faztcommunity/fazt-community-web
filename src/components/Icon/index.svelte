621121cb6de8f0d2e2fb5b3283c232c27b40baad
erianvc <erianvc.dev@gmail.com>
2021-05-29 02:19:02 -0500

    feat: add icon component

---
+++ src/components/Icon/index.svelte
@@ -0,0 +1,45 @@
+<script>
+    import api from 'assets/icons/api.svg'
+    import bot from 'assets/icons/bot.svg'
+    import eye from 'assets/icons/eye.svg'
+    import web from 'assets/icons/web.svg'
+    import logo from 'assets/icons/logo.svg'
+    import check from 'assets/icons/check.svg'
+    import cross from 'assets/icons/cross.svg'
+    import error from 'assets/icons/error.svg'
+    import github from 'assets/icons/github.svg'
+    import discord from 'assets/icons/discord.svg'
+    import success from 'assets/icons/success.svg'
+    import warning from 'assets/icons/warning.svg'
+    import database from 'assets/icons/database.svg'
+    import eyeclose from 'assets/icons/eyeclose.svg'
+
+    export let name
+    export let size = 24
+
+    const AVAIBLE_ICONS = {
+        api,
+        bot,
+        eye,
+        web,
+        logo,
+        check,
+        cross,
+        error,
+        github,
+        discord,
+        success,
+        warning,
+        database,
+        eyeclose,
+    }
+</script>
+
+<svelte:component
+    this={AVAIBLE_ICONS[
+        Object.keys(AVAIBLE_ICONS).includes(name) ? name : 'error'
+    ]}
+    width={size}
+    height={size}
+    {...$$restProps}
+    on:click />
--- src/routes/components.svelte
+++ src/routes/components.svelte
@@ -1,4 +1,5 @@
 <script>
+    import Icon from '@components/Icon'
     import Button from '@components/Button'
 </script>
 
@@ -15,6 +16,23 @@
         <br /><br />
         <Button primary large disabled>Unirme a la comunidad</Button>
         <Button large disabled>Ver proyectos</Button>
+    </article>
+    <article>
+        <Icon />
+        <Icon name="api" size="32" />
+        <Icon name="bot" size="32" />
+        <Icon name="eye" size="32" />
+        <Icon name="web" size="32" />
+        <Icon name="logo" size="32" />
+        <Icon name="check" size="32" />
+        <Icon name="cross" size="32" />
+        <Icon name="error" size="32" />
+        <Icon name="github" size="32" />
+        <Icon name="discord" size="32" />
+        <Icon name="success" size="32" />
+        <Icon name="warning" size="32" />
+        <Icon name="database" size="32" />
+        <Icon name="eyeclose" size="32" />
     </article>
 </section>
 
