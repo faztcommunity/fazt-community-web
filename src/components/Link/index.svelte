dd37348351583947d7d9b94a0d8f33360c625d84
erianvc <erianvc.dev@gmail.com>
2021-05-29 23:55:30 -0500

    feat: add link component

---
+++ src/components/Link/index.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let btn = false
+    export let label = null
+    export let fill = false
+    export let primary = false
+    export let large = false
+    export let medium = false
+    export let small = false
+    export let external = false
+    export { href as to }
+
+    let href = '/'
+</script>
+
+<a
+    {href}
+    class:btn
+    class:primary
+    class:fill
+    class:large
+    class:medium
+    class:small
+    aria-label={label}
+    target={external ? '_blank' : null}
+    rel={external ? 'external noopener noreferrer' : 'prefetch'}
+    {...$$restProps}>
+    <slot />
+</a>
