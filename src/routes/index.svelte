3c046369d95eebdee1cac89a18a74f899e9825c3
erianvc <erianvc.dev@gmail.com>
2021-06-01 00:54:20 -0500

    feat: add contributors section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,10 @@
     import Hero from './_sections/Hero.svelte'
     import About from './_sections/About.svelte'
     import Projects from './_sections/Projects.svelte'
+    import Contributors from './_sections/Contributors.svelte'
 </script>
 
 <Hero />
 <About />
 <Projects />
+<Contributors />
