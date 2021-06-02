4317ef4fee934ad50fcb6f7af057968edfaed933
erianvc <erianvc.dev@gmail.com>
2021-06-01 21:35:04 -0500

    feat: add joinus section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,9 +3,11 @@
     import About from './_sections/About.svelte'
     import Projects from './_sections/Projects.svelte'
     import Contributors from './_sections/Contributors.svelte'
+    import JoinUs from './_sections/JoinUs.svelte'
 </script>
 
 <Hero />
 <About />
 <Projects />
 <Contributors />
+<JoinUs />
