1f906fe24103a24bcb91960d2b2ac6645170ae07
erianvc <erianvc.dev@gmail.com>
2021-06-01 22:16:05 -0500

    feat: add footer component

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,11 +1,13 @@
 <script>
     import Navbar from '@components/Navbar'
+    import Footer from '@components/Footer'
 </script>
 
 <Navbar />
 <main>
     <slot />
 </main>
+<Footer />
 
 <style lang="scss">
     @import 'src/styles/utils';
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const socials = {
+    DISCORD: 'https://discord.gg/hfQsNGNcvQ',
+    GITHUB: 'https://github.com/faztcommunity',
+}
