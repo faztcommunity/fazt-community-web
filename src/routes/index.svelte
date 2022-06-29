6c5adcdf9be223cb75351803ac861860d0ffeed6
erianvc <erianvc.dev@gmail.com>
2022-06-29 09:05:20 -0500

    refactor: remove projects section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,13 @@
 <script>
     import Hero from './_sections/Hero.svelte'
     import About from './_sections/About.svelte'
-    import Projects from './_sections/Projects.svelte'
+    // import Projects from './_sections/Projects.svelte'
     import Contributors from './_sections/Contributors.svelte'
     import JoinUs from './_sections/JoinUs.svelte'
 </script>
 
 <Hero />
 <About />
-<Projects />
+<!-- <Projects /> -->
 <Contributors />
 <JoinUs />
