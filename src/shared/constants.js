f1a6554cb1e68af20ac9b06fdc5657504263635a
erianvc <erianvc.dev@gmail.com>
2022-09-20 01:06:01 -0500

    refactor: constant link in code

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,2 +1,2 @@
-export const GITHUB_LINK = import.meta.env.PUBLIC_GITHUB_LINK
-export const DISCORD_LINK = import.meta.env.PUBLIC_DISCORD_LINK
+export const GITHUB_LINK = 'https://github.com/faztcommunity'
+export const DISCORD_LINK = 'https://discord.com/invite/JpnY3YqzCN'
